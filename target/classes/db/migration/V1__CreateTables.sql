493f7c88c6e131defe75dcf51eb8c879e91b5fdb
Guilherme_David <guilherme.david@2x3.com.br>
2021-05-26 12:41:22 -0300

    atualizando banco

--- target/classes/db/migration/V1__CreateTables.sql
+++ target/classes/db/migration/V1__CreateTables.sql
@@ -1,50 +1,53 @@
-set foreign_key_checks=0;
 
+-- SQLINES LICENSE FOR EVALUATION USE ONLY
 CREATE TABLE users (
-    id INT(20) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    id INT CHECK (id > 0) IDENTITY PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
     email VARCHAR(100) NOT NULL,
     password VARCHAR(255) NOT NULL,
-    is_admin BOOLEAN NOT NULL,
+    is_admin BIT NOT NULL,
 
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    deleted_at TIMESTAMP NULL DEFAULT NULL
+    created_at DATETIME2(0) DEFAULT GETDATE(),
+    deleted_at DATETIME2(0) NULL DEFAULT NULL
 );
 
+-- SQLINES LICENSE FOR EVALUATION USE ONLY
 CREATE TABLE obras (
-    id INT(20) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    prazo INT(6),
-    valor_total INT(20),
-    bonus_entrega INT(20),
-    tempo_limite INT(20),
-    user_id INT(20) UNSIGNED NOT NULL,
+    id INT CHECK (id > 0) IDENTITY PRIMARY KEY,
+    prazo INT,
+    valor_total INT,
+    bonus_entrega INT,
+    tempo_limite INT,
+    user_id INT CHECK (user_id > 0) NOT NULL,
 
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    deleted_at TIMESTAMP NULL DEFAULT NULL
+    created_at DATETIME2(0) DEFAULT GETDATE(),
+    deleted_at DATETIME2(0) NULL DEFAULT NULL
 );
 
 
+-- SQLINES LICENSE FOR EVALUATION USE ONLY
 CREATE TABLE bonus (
-    id INT(20) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    obras_id INT(20) UNSIGNED NOT NULL,
-    dias INT(6),
-    valor INT(20),
+    id INT CHECK (id > 0) IDENTITY PRIMARY KEY,
+    obras_id INT CHECK (obras_id > 0) NOT NULL,
+    dias INT,
+    valor INT,
 
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    deleted_at TIMESTAMP NULL DEFAULT NULL,
+    created_at DATETIME2(0) DEFAULT GETDATE(),
+    deleted_at DATETIME2(0) NULL DEFAULT NULL,
 
     CONSTRAINT FK_bonus_obras_id FOREIGN KEY (obras_id) REFERENCES obras(id)
 );
 
+-- SQLINES LICENSE FOR EVALUATION USE ONLY
 CREATE TABLE equipes (
-    id INT(20) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    obras_id INT(20) UNSIGNED NOT NULL,
-    quantidade_funcionarios INT(20),
+    id INT CHECK (id > 0) IDENTITY PRIMARY KEY,
+    obras_id INT CHECK (obras_id > 0) NOT NULL,
+    quantidade_funcionarios INT,
 
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    deleted_at TIMESTAMP NULL DEFAULT NULL,
+    created_at DATETIME2(0) DEFAULT GETDATE(),
+    deleted_at DATETIME2(0) NULL DEFAULT NULL,
 
     CONSTRAINT FK_equipes_obras_id FOREIGN KEY (obras_id) REFERENCES obras(id)
 );
 
-set foreign_key_checks=1;
+
